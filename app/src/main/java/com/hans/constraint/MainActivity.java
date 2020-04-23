147c84277dba6638f2182bae43ef3d95039b39a9
xujiang202839 <1215266274@qq.com>
2020-04-23 14:40:48 +0800

    第一次通过Android studio setting github设置账号，VCS share project on Github

--- app/src/main/java/com/hans/constraint/MainActivity.java
+++ app/src/main/java/com/hans/constraint/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //再次提交
         bt_num = findViewById(R.id.bt_num);
         tv_content = findViewById(R.id.tv_content);
 
